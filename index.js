7e6dee15612376558da97226cb2bb6c0cd33a1d0
DanielaVieira <daniela.vieirac@gmail.com>
2021-05-17 17:41:23 -0300

    Primeiro commit do sistema

---
+++ index.js
@@ -0,0 +1,13 @@
+$(()=>{
+  $("#btnAdicionar").click(() =>{
+      $("#tblDados tbody").append(`<tr>
+      <td>-- # -- </td>
+      <td>Nome</td>
+      <td>-- sobrenome --</td>
+      <td>01/01/1980</td>
+
+      </tr>`)
+  })
+    
+
+})
